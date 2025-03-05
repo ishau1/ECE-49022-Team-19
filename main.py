2e555e079764229794f0bb11c86f7d9f5f356ed8
ishau1 <122313874+ishau1@users.noreply.github.com>
2025-03-05 10:54:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import copy
-import math
 import time
 import cv2
 import Functions as Fun1
+import statistics
 
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from PIL import Image
@@ -13,12 +13,13 @@ import matplotlib.pyplot as plt
 #  Function that creates the grayscale image of the Resistor where the body of the resistor appears as white
 def color_array_filler(dimensions1, image_array1, color_array1):
     index1 = 0  #Itteration variable
-    while index1 < dimensions1[0]:
-        index2 = 0
-        while index2 < dimensions1[1]:
+    while index1 < dimensions1[0]:  #Iterates through the height of the image
+        index2 = 0  #Itteration variable
+        while index2 < dimensions1[1]:  #Iterates through the width of the image
             color_array1[index1][index2] = Fun1.colordeterm(image_array1[index1][index2][0],
                                                             image_array1[index1][index2][1],
                                                             image_array1[index1][index2][2])
+           # Fills in 2D grayscale matrix which will be used to identify body of the Resistor
             index2 += 1  #Increases increment by 1
         index1 += 1  #Increases increment by 1
     return None
@@ -26,15 +27,15 @@ def color_array_filler(dimensions1, image_array1, color_array1):
 
 #  Function that determines the slope and y-intercept of the line that bisects the body of the Resistor
 def slope_determ(x_coord1, y_coord1, x_coord2, y_coord2):
-    if x_coord1 == x_coord2:
-        return 1
+    if x_coord1 == x_coord2:  #Checks if x coordinates are identical as slope would be infinity in that case
+        return 0  #Returns 0 because slope will be 0 when iterating across y-axis values
     slope1 = (y_coord2 - y_coord1)/(x_coord2 - x_coord1)
-    if abs(slope1) > 1.0:
+    if abs(slope1) > 1.0:  #Checks if slope is greater than 1 so it can have its inverse be the slope
         slope1 = 1/slope1
-    if abs(x_coord2 - x_coord1) > abs(y_coord2 - y_coord1):
-        b = -slope1*x_coord1 + y_coord1
+    if abs(x_coord2 - x_coord1) > abs(y_coord2 - y_coord1):  #Checks if Resistor is more horizontal or vertical
+        b = -slope1*x_coord1 + y_coord1  #Determines y-intercept is Resistor is more horizontal
     else:
-        b = -slope1*y_coord1 + x_coord1
+        b = -slope1*y_coord1 + x_coord1  #Determines y-intercept is Resistor is more vertical
     return slope1, b
 
 
@@ -42,35 +43,54 @@ def slope_determ(x_coord1, y_coord1, x_coord2, y_coord2):
 #  Function that determines the colors associated with that pixel using an RGB function as well as an HSV function
 def color_array_line_plotter(slope1, y_int1, x_coord1, y_coord1, x_coord2, y_coord2, orientation3, color_array1,
                              image_array1, color_array2, hsv_array1, hsv_color_array):
-    if orientation3:
-        starter_index1 = y_coord1
-        while starter_index1 < y_coord2:
+    dimensions1 = image_array1.shape
+    if orientation3:  #Orientation value that determines the Resistor to be more Vertical
+        starter_index1 = y_coord1  #The y-axis value (row) of the first pixel in the line that bisects Resistor
+        while starter_index1 < y_coord2:  #Will run through all y-axis values until it reaches end of line of pixels
             starter_index2 = int(slope1*starter_index1 + y_int1)
+            # The x-axis value (column) of the first pixel in the line that bisects Resistor
             color_array2[starter_index1][starter_index2] = 128
+            # Assigns the pixel in the line gray value
             color_array1.append(Fun1.color_determ_color_chart_return(
                 image_array1[starter_index1][starter_index2][0],
                 image_array1[starter_index1][starter_index2][1],
                 image_array1[starter_index1][starter_index2][2]))
-            image_array1[starter_index1][starter_index2][3] = 0
+
+            if color_array1[-1] == 11:
+                Fun1.pixel_glare_color_determ(starter_index2, starter_index1, slope1, orientation3, color_array1,
+                                              image_array1, 6, dimensions1[0], dimensions1[1])
+
+            # Function that adds the pixels color determined by RGB to an array
+            image_array1[starter_index1][starter_index2][1] = 255  #Assigns the pixel in the line green value
             hsv_color_array.append(Fun1.color_determ_hsv(int(hsv_array1[starter_index1][starter_index2][0]),
                                                          int(hsv_array1[starter_index1][starter_index2][1]),
                                                          int(hsv_array1[starter_index1][starter_index2][2])))
-            hsv_array1[starter_index1][starter_index2][1] = 0
+            # Function that adds the pixels color determined by HSV to an array
+            hsv_array1[starter_index1][starter_index2][1] = 0  #Assigns the pixel in the line 0 saturation value
             starter_index1 += 1  #Increases increment by 1
-    else:
-        starter_index1 = x_coord1
+    else:  #Orientation value determined the Resistor to be more Horizontal
+        starter_index1 = x_coord1  #The x-axis value (column) of the first pixel in the line that bisects Resistor
         while starter_index1 < x_coord2:
             starter_index2 = int(slope1*starter_index1 + y_int1)
+            # The y-axis value (row) of the first pixel in the line that bisects Resistor
             color_array2[starter_index2][starter_index1] = 128
+            # Assigns the pixel in the line gray value
             color_array1.append(Fun1.color_determ_color_chart_return(
                 image_array1[starter_index2][starter_index1][0],
                 image_array1[starter_index2][starter_index1][1],
                 image_array1[starter_index2][starter_index1][2]))
-            image_array1[starter_index2][starter_index1][3] = 0
+
+            if color_array1[-1] == 11:
+                Fun1.pixel_glare_color_determ(starter_index1, starter_index2, slope1, orientation3, color_array1,
+                                              image_array1, 4, dimensions1[0], dimensions1[1])
+
+            # Function that adds the pixels color determined by RGB to an array
+            image_array1[starter_index2][starter_index1][1] = 255  #Assigns the pixel in the line green value
             hsv_color_array.append(Fun1.color_determ_hsv(int(hsv_array1[starter_index2][starter_index1][0]),
                                                          int(hsv_array1[starter_index2][starter_index1][1]),
                                                          int(hsv_array1[starter_index2][starter_index1][2])))
-            hsv_array1[starter_index2][starter_index1][1] = 0
+            # Function that adds the pixels color determined by HSV to an array
+            hsv_array1[starter_index2][starter_index1][1] = 0  #Assigns the pixel in the line 0 saturation value
             starter_index1 += 1  #Increases increment by 1
     return None
 
@@ -78,83 +98,72 @@ def color_array_line_plotter(slope1, y_int1, x_coord1, y_coord1, x_coord2, y_coo
 #  Function that determines the individual integers correlated with each band
 #  Function also determines whether the integers are in reverse order or not
 def band_integer_determ(long_array, short_array):
-    index1 = 0
-    index2 = 0
-    num_pix_start = 0
-    num_pix_end = 0
-    length1 = len(long_array)
-    while index1 < (length1 - 3):
-        if long_array[index1] != long_array[index1 + 1]:
-            if index1 < length1 - 2:
-                if long_array[index1 + 1] == long_array[index1 + 2] and \
-                        long_array[index1 + 1] == long_array[index1 + 3] and \
-                        long_array[index1 + 1] != long_array[index1 - 2]:
-                    short_array.append(long_array[index1 + 1])
+    index1 = 0  #Itteration variable
+    length1 = len(long_array)  #Determines the length of the array
+    while index1 < (length1 - 4):  #Iterates through all color values of array except final 4
+        if long_array[index1] != long_array[index1 + 1]:  #Checks for change in pixel value
+            array1 = long_array[index1 + 1:index1 + 5]  #Creates an array of the next 4 color values in the array
+            median1 = statistics.median(array1)  #Determines the median of the new array
+            sum1 = array1.count(median1)  #Determines the number of times the median value of the new array occurs
+            if sum1 >= 3 and median1 == long_array[index1 + 1]:  #Determines if the change in pixels should result in
+                # new pixel being added to the short_array
+                short_array.append(long_array[index1 + 1])  #Adds integer to short_array
+                if index1 <= (length1 - 11) and not (long_array[index1 + 1] == 12 or
+                                                     long_array[index1 + 1] == 11 or long_array[index1 + 1] == 13):
+                    # Checks if index value is less than 10 from max value
+                    index1 += 10  #Jumps 10 pixel in order to leap pass pixel band which it is currently in
         index1 += 1  #Increases increment by 1
 
-
     # print(long_array)
-    element_to_remove = 12
-    while element_to_remove in short_array:
+    element_to_remove = 12  #Element that represents light blue pixels
+    while element_to_remove in short_array:  #Function that removes all light blue pixels from array
         short_array.remove(element_to_remove)
 
+    element_to_remove = 11  #Element that represents light blue pixels
+    while element_to_remove in short_array:  #Function that removes all light blue pixels from array
+        short_array.remove(element_to_remove)
 
-    while index2 < length1 - 2:
-        if long_array[index2] != element_to_remove and long_array[index2 + 1] != element_to_remove \
-                and long_array[index2 + 2] != element_to_remove:
-            index2 = length1
-        else:
-            num_pix_start += 1
-            index2 += 1  #Increases increment by 1
-    while index2 > 2:
-        if long_array[index2 - 1] != element_to_remove and long_array[index2 - 2] != element_to_remove \
-                and long_array[index2 - 3] != element_to_remove:
-            index2 = 0
-        else:
-            num_pix_end += 1
-            index2 -= 1  #Decreases increment by 1
+    element_to_remove = 13  #Element that represents light blue pixels
+    while element_to_remove in short_array:  #Function that removes all light blue pixels from array
+        short_array.remove(element_to_remove)
 
-    # print(num_pix_start, num_pix_end)
-
-    if num_pix_start > num_pix_end:
-        return True  #Returns True if Color Bands Array is in reverse order
-    else:
-        return False  #Returns False if Color Band Array is in correct order
+    return True  #Currently outdated, does not affect main functions output
 
 
 #  Function that determines if the body of the resistor is in a more horizontal or vertical direction
-def orientation_determ(grayscale_array, length1, width1, pixel1):
-    index1 = 0
-    threshold1 = pixel1*255
-    right = 0
-    left = 0
-    top = 0
-    bottom = 0
-    while index1 < length1:
-        if grayscale_array[index1].sum() >= threshold1:
-            top = index1
-            index1 = length1
+def orientation_determ(grayscale_array, width1, length1, pixel1):
+    index1 = 0  #Itteration variable
+    threshold1 = pixel1*255  #Arbitrarily defined threshold for number of pixels
+    right = 0  #right most pixel index
+    left = 0  #left most pixel index
+    top = 0  #top most pixel index
+    bottom = 0  #bottom most pixel index
+    while index1 < length1:  #While loop that will iterate through all y-axis (row) values
+        if grayscale_array[index1].sum() >= threshold1:  #Checks if sum of row of pixels exceeds threshold
+            top = index1  #Assigns index to the first row from the top that exceeds threshold
+            index1 = length1  #Assigns index1 to end value to end while loop
+        else:
+            index1 += 1  #Increases increment by 1
+    index1 = 0  #Returns Iteration variable to 0
+    while index1 < length1:  #While loop that will iterate through all y-axis (row) values
+        if grayscale_array[length1 - index1 - 1].sum() >= threshold1:  #Checks if sum of row of pixels exceeds threshold
+            bottom = length1 - index1 - 1  #Assigns index to the first row from the bottom that exceeds threshold
+            index1 = length1  #Assigns index1 to end value to end while loop
+        else:
+            index1 += 1  #Increases increment by 1
+    index1 = 0  #Returns Iteration variable to 0
+    while index1 < width1:  #While loop that will iterate through all x-axis (column) values
+        if grayscale_array[:, index1].sum() >= threshold1:  #Checks if sum of column of pixels exceeds threshold
+            left = index1  #Assigns index to the first column from the left that exceeds threshold
+            index1 = width1  #Assigns index1 to end value to end while loop
         else:
             index1 += 1  #Increases increment by 1
     index1 = 0
-    while index1 < length1:
-        if grayscale_array[length1 - index1 - 1].sum() >= threshold1:
-            bottom = length1 - index1 - 1
-            index1 = length1
-        else:
-            index1 += 1  #Increases increment by 1
-    index1 = 0
-    while index1 < width1:
-        if grayscale_array[:, index1].sum() >= threshold1:
-            left = index1
-            index1 = width1
-        else:
-            index1 += 1  #Increases increment by 1
-    index1 = 0
-    while index1 < width1:
+    while index1 < width1:  #While loop that will iterate through all x-axis (column) values
         if grayscale_array[:, width1 - index1 - 1].sum() >= threshold1:
-            right = width1 - index1 - 1
-            index1 = width1
+            # Checks if sum of row of pixels exceeds threshold
+            right = width1 - index1 - 1  #Assigns index to the first column from the right that exceeds threshold
+            index1 = width1  #Assigns index1 to end value to end while loop
         else:
             index1 += 1  #Increases increment by 1
 
@@ -167,12 +176,12 @@ def orientation_determ(grayscale_array, length1, width1, pixel1):
 
 #  Function that determines the x and y coordinates of the 4 vertices that define location of the body of the Resistor
 def line_bisection_coords1(color_array2, pixel_var, orientation_var, height1, width1):
-    index1 = 0
-    threshold1 = pixel_var*255*3
-    row_top = 0
-    row_bottom = 0
-    col_left = 0
-    col_right = 0
+    index1 = 0  #Itteration variable
+    threshold1 = pixel_var*255*3  #Arbitrary threshold voltage
+    row_top = 0  #Top y-axis index value
+    row_bottom = 0  #Bottom y-axis index value
+    col_left = 0 #Left most x-axis index value
+    col_right = 0 #Right most x-axis index value
     if orientation_var:  #Checks if orientation_var is True
         while index1 < height1:
             if color_array2[index1].sum() >= threshold1:
@@ -231,11 +240,13 @@ def line_bisection_coords1(color_array2, pixel_var, orientation_var, height1, wi
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     start_time = time.time()  #Identifies the start time
-    image = Image.open('R47.png')  #Assigns png image to a variable
+    image = Image.open('R_test11.jpg')  #Assigns png image to a variable
     image_array = np.array(image)   #Takes png and converts it to a 3D number array
     image_array_copy = copy.deepcopy(image_array)  #Creates an identical matrix of image_array
     dimensions = image_array.shape  #Obtains dimensions of array, 1st number is length, 2nd number is width
     hsv_image = cv2.cvtColor(image_array_copy, cv2.COLOR_BGR2HSV)
+
+
 
     # 3rd is depth
     color_array = np.zeros((dimensions[0], dimensions[1]))  #Creates an array of zeros with same length and width as the
@@ -245,9 +256,9 @@ if __name__ == '__main__':
     color_array_filler(dimensions, image_array, color_array)  #Runs function that blacks out all pixels except for
     # except for pixel in the body of the resistor
 
-    pixel_value_var = 5  #Helper variable that varies based on number of pixels in the body of the resistor
+    pixel_value_var = 10  #Helper variable that varies based on number of pixels in the body of the resistor
 
-    orientation1 = orientation_determ(color_array, dimensions[0], dimensions[1], pixel_value_var)
+    orientation1 = orientation_determ(color_array, dimensions[1], dimensions[0], pixel_value_var)
     # Returns True if resistor is more Vertical, Returns False if resistor is more horizontal
     coords2 = line_bisection_coords1(color_array, pixel_value_var, orientation1, dimensions[0], dimensions[1])
     # Array containing 8 values representing the x and y of the 4 vertices of the body of the resistor
@@ -278,9 +289,9 @@ if __name__ == '__main__':
                              color_array_color_chart, image_array, color_array, hsv_image, color_array_color_chart_hsv)
     # Function that plots bisecting line in gray on the color array
 
-    print(color_array_color_chart_hsv)
-    print(color_array_color_chart)
-
+    matrix_colors = np.vstack((color_array_color_chart_hsv, color_array_color_chart))  #Combines my two arrays with
+    # With colors based on the resistor color chart. Top is HSV and bottom is RGB
+    print(matrix_colors)
 
     orientation_band = band_integer_determ(color_array_color_chart, color_array_color_chart_bands_only)
     # Function that determines the orientation of the color band.
@@ -292,7 +303,7 @@ if __name__ == '__main__':
 
     print(color_array_color_chart_bands_only)  #Prints the array of the integers associated with each color band
 
-
+    """
     if len(color_array_color_chart_bands_only) > 3:  #Checks if resistor is 5 band or greater
         resistance_value = (color_array_color_chart_bands_only[0]*100 + color_array_color_chart_bands_only[1]*10
                             + color_array_color_chart_bands_only[2]) * 10**color_array_color_chart_bands_only[-1]
@@ -303,16 +314,20 @@ if __name__ == '__main__':
         # Calculates resistance value of 4 band resistor
     else:
         resistance_value = False  #Returns false in case of poor determination of color bands
-
-    bin_number = Fun1.bin_determ(color_array_color_chart_bands_only)
-
     print(resistance_value)  #Prints the resistance value of the resistor
-    print("The bin number is", bin_number)
-
+    """
 
     end_time = time.time()  #End time variable
 
     print(end_time - start_time)  #Displays time taken for program to run
     plt.imshow(image_array, cmap='gray')
+    #plt.imshow(hsv_image, cmap='gray')
+    #plt.imshow(color_array, cmap='gray')
 
     plt.show()
+
+    bin_number = Fun1.bin_determ(color_array_color_chart_bands_only)
+    print("The bin number is", bin_number)
+
+    #plt.imshow(color_array, cmap='gray')
+    #plt.show()
