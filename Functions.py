01bad316021bfab04c586484407775cabb052493
ishau1 <122313874+ishau1@users.noreply.github.com>
2025-04-11 10:58:29 -0400

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,7 +1,6 @@
 import numpy as np
 import math
 import statistics
-from scipy.stats import linregress
 
 
 
@@ -33,17 +32,20 @@ def color_determ_color_chart_return(red, green, blue):
     green = int(green)
     blue = int(blue)
     sum1 = red + green + blue
-    if sum1 >= 650:
+    if sum1 >= 650: # Determines White
         return 13
-    elif red >= 120 and green >= 120 and blue >= 120 and sum1 >= 400:
+    elif red >= 120 and green >= 120 and blue >= 120 and sum1 >= 400:  # Determines Glare
         return 11
-    elif (red - green) >= 25 and (red - blue) >= 25 and (red <= 160 and green <= 75) and abs(green - blue) <= 20 and red >= 40:  #Determines Red
+    elif (red - green) >= 25 and (red - blue) >= 25 and (red <= 160 and green <= 75) and abs(green - blue) <= 20 \
+            and red >= 40:  #Determines Red
         return 2
-    elif blue >= 160 and green >= 100 and 2*green > blue:  #Determines Resistor Background Color Light Blue
+    elif blue >= 160 and green >= 100 and (2*green > blue):  #Determines Resistor Background Color Light Blue
         return 12
     elif sum1 <= 50:
         if (red >= 5*green and red >= 5*blue) or (red - green >= 5 and red - blue >= 5):
             return 1
+        elif green > blue and blue > red and (green - red >= 15):
+            return 5
         else:
             return 0
     elif sum1 < 100 and abs(red - blue) <= 15 and abs(red - green) <= 15:  #Determines Black
@@ -54,9 +56,11 @@ def color_determ_color_chart_return(red, green, blue):
         return 1
     elif red - green >= 15 and (green - blue >= 20) and (red - blue >= 40) and red >= 75:  #Determines Orange
         return 3
-    elif (green - red) >= 25 and (green - blue) >= 80:  #Determines Yellow
+    elif (green - red) >= 25 and (green - blue) >= 70:  #Determines Yellow
         return 4
-    elif green >= 3*red and green >= 2*blue and green >= 40 or (green - red >= 25 and green - blue >= 25 and green >= 40):  #Determines Green
+    elif green >= 3*red and green >= 2*blue and green >= 40 or (green - red >= 25
+                                                                and green - blue >= 25 and green >= 40):
+        # Determines Green
         return 5
     elif blue >= 4*red and green <= 60 and blue >= 105 and (red + green >= 10):  #Determines Blue
         return 6
@@ -64,15 +68,25 @@ def color_determ_color_chart_return(red, green, blue):
         return 7
     #  Determines Gray
     elif abs(red - blue) <= 25 and abs(red - green) <= 25 and abs(blue - green) <= 25 and sum1 >= 105 and green >= red:
+        # Determines Gray
         return 8
     elif sum1 >= 75 and abs(red - blue) <= 15 and abs(red - green) <= 15 and abs(blue - green) <= 15 and green > red:
+        # Determines Gray
         return 8
-    elif sum1 >= 150 and green > red and blue > red and abs(green - blue) <= 15 and abs(green - red) <= 50 and abs(blue - red) <= 50 and red >= 45:
+    elif (sum1 >= 150 and green > red and blue > red and abs(green - blue) <= 15 and abs(green - red) <= 50 and sum1 <= 300
+          and abs(blue - red) <= 50 and red >= 45):
+        # Determines Gray
         return 8
-    elif red <= 60 and green >= 50 and blue >= 65 and green <= 135 and blue <= 150 and ((green - red) >= 20) and ((blue - red) >= 35) and red >= 10 and (abs(blue - green) <= 25):
+    elif (red <= 60 and green >= 50 and blue >= 65 and green <= 135 and blue <= 150 and ((green - red) >= 20)
+          and ((blue - red) >= 35) and red >= 10 and (abs(blue - green) <= 25)):
+        # Determines Gray
         return 8
-    elif green >= 4*red and green >= blue and green >= 35 and (green - red >= 30):
+    elif green >= 4*red and green >= blue and green >= 35 and (green - red >= 30):  # Determines Green
         return 5
+    elif green > blue and blue > red and green <= 75 and green >= 20 and (green - red >= 15):
+        return 5
+    elif sum1 <= 75 and (red - 10 >= green) and (red - 10 >= blue):
+        return 1
     else:  #Determines pixel color to be none of the above
         return 11
 
@@ -104,12 +118,28 @@ def color_determ_hsv(hue, saturation, value):
 
 
 #  Function to determine which bin the resistor is to be sent to
-def bin_determ(band_array):
+def bin_determ(band_array, ratio1):
+    count5s = band_array.count(5)
+    count3s = band_array.count(3)
+    count8s = band_array.count(8)
+    count2s = band_array.count(2)
     count1s = band_array.count(1)
     count0s = band_array.count(0)
     sum01s = count0s + count1s
+    sum012s = sum01s + count2s
     if len(band_array) < 3 :  #Checks if array is empty
         return 7  #Integer to let microprocessor know the no bin was determined
+    if (ratio1 <= 1.2 or ratio1 > 1.7) and count8s == 0 and count3s < 2:
+        if count5s and ratio1 > 1.7:
+            return 9
+        else:
+            return 8
+
+    elif count5s == 1 and sum012s == 4 and ratio1 <= 1.4:
+        # Checks for edge case where 2 resistors have exact same color band
+        return 8
+    elif count3s == 1 and count2s == 1 and sum012s >= 3:
+        return 10
     elif band_array[0] == 8 and band_array[1] == 2 or band_array[-1] == 8 and band_array[-2] == 2:  #Send to bin 8
         return 15
     elif band_array[0] == 6 and band_array[1] == 8 or band_array[-1] == 6 and band_array[-2] == 8:  #Send to bin 7
@@ -117,6 +147,8 @@ def bin_determ(band_array):
     elif band_array[0] == 5 and band_array[1] == 6 or band_array[-1] == 5 and band_array[-2] == 6:  #Send to bin 6
         return 13
     elif band_array[0] == 4 and band_array[1] == 7 or band_array[-1] == 4 and band_array[-2] == 7:  #Send to bin 5
+        return 12
+    elif band_array[0] == 4 and band_array[1] == 6 or band_array[-1] == 4 and band_array[-2] == 6:  #Send to bin 5
         return 12
     elif band_array[0] == 3 and band_array[1] == 3 or band_array[-1] == 3 and band_array[-2] == 3:  #Send to bin 4
         return 11
@@ -231,13 +263,14 @@ def pixel_glare_color_determ(x_coord, y_coord, slope, orientation, color_array, 
                     image_array[new_coord_y2][x_coord - distance_new_pixel_var][0],
                     image_array[new_coord_y2][x_coord - distance_new_pixel_var][1],
                     image_array[new_coord_y2][x_coord - distance_new_pixel_var][2])
+            image_array[new_coord_y1][x_coord + distance_new_pixel_var][0] = 255
         else:
-            new_pixel_color1 = 11
+            new_pixel_color1 = 13
             new_pixel_color2 = color_determ_color_chart_return(
                     image_array[new_coord_y2][x_coord - distance_new_pixel_var][0],
                     image_array[new_coord_y2][x_coord - distance_new_pixel_var][1],
                     image_array[new_coord_y2][x_coord - distance_new_pixel_var][2])
-        image_array[new_coord_y1][x_coord + distance_new_pixel_var][0] = 255
+            image_array[new_coord_y1][x_coord][0] = 255
         image_array[new_coord_y2][x_coord - distance_new_pixel_var][0] = 255
         #print(new_coord_y1, x_coord + distance_new_pixel_var, new_pixel_color1, distance_var, 1, "V")
         #print(new_coord_y2, x_coord - distance_new_pixel_var, new_pixel_color2, distance_var, 2, "V")
@@ -253,13 +286,14 @@ def pixel_glare_color_determ(x_coord, y_coord, slope, orientation, color_array, 
                     image_array[y_coord - distance_new_pixel_var][new_coord_x2][0],
                     image_array[y_coord - distance_new_pixel_var][new_coord_x2][1],
                     image_array[y_coord - distance_new_pixel_var][new_coord_x2][2])
+            image_array[y_coord + distance_new_pixel_var][new_coord_x1][0] = 255
         else:
-            new_pixel_color1 = 11
+            new_pixel_color1 = 13
             new_pixel_color2 = color_determ_color_chart_return(
                     image_array[y_coord - distance_new_pixel_var][new_coord_x2][0],
                     image_array[y_coord - distance_new_pixel_var][new_coord_x2][1],
                     image_array[y_coord - distance_new_pixel_var][new_coord_x2][2])
-        image_array[y_coord + distance_new_pixel_var][new_coord_x1][0] = 255
+            image_array[y_coord][new_coord_x1][0] = 255
         image_array[y_coord - distance_new_pixel_var][new_coord_x2][0] = 255
         #print(y_coord + distance_new_pixel_var, new_coord_x1, new_pixel_color1, distance_var, 1, "H")
         #print(y_coord - distance_new_pixel_var, new_coord_x2, new_pixel_color2, distance_var, 2, "H")
@@ -277,7 +311,8 @@ def pixel_glare_color_determ(x_coord, y_coord, slope, orientation, color_array, 
     elif sum01s == 2 and color_orig != 0 and color_orig != 1:
         pixel_glare_color_determ(x_coord, y_coord, slope, orientation, color_array, image_array, distance_var + 4, height1, width1, 0)
     elif sum12s == 2:
-        pixel_glare_color_determ(x_coord, y_coord, slope, orientation, color_array, image_array, distance_var + 4, height1, width1, 1)
+        color_array[-1] = 2
+        #pixel_glare_color_determ(x_coord, y_coord, slope, orientation, color_array, image_array, distance_var + 4, height1, width1, 1)
     else:
         pixel_glare_color_determ(x_coord, y_coord, slope, orientation, color_array, image_array, distance_var + 4, height1, width1, color_orig)
     return None
@@ -285,7 +320,7 @@ def pixel_glare_color_determ(x_coord, y_coord, slope, orientation, color_array, 
 #  Function that determines the x and y coordinates of the 4 vertices that define location of the body of the Resistor
 def line_bisection_coords1(color_array2, pixel_var, orientation_var, height1, width1):
     index1 = 0  #Itteration variable
-    threshold1 = pixel_var*255*3  #Arbitrary threshold voltage
+    threshold1 = pixel_var*255*2.5  #Arbitrary threshold voltage
     row_top = 0  #Top y-axis index value
     row_bottom = 0  #Bottom y-axis index value
     col_left = 0 #Left most x-axis index value
@@ -308,12 +343,18 @@ def line_bisection_coords1(color_array2, pixel_var, orientation_var, height1, wi
         top_right_y = row_top
         bottom_left_y = row_bottom
         bottom_right_y = row_bottom
-        nonzero_indices_top = np.nonzero(color_array2[row_top])[0]
-        nonzero_indices_bottom = np.nonzero(color_array2[row_bottom])[0]
-        top_left_x = int(nonzero_indices_top[0])
-        top_right_x = int(nonzero_indices_top[-1])
-        bottom_left_x = int(nonzero_indices_bottom[0])
-        bottom_right_x = int(nonzero_indices_bottom[-1])
+        nonzero_array_top = np.nonzero(color_array2[row_top])[0]
+        nonzero_array_bottom = np.nonzero(color_array2[row_bottom])[0]
+        if len(nonzero_array_top) <= 1 or len(nonzero_array_bottom) <= 1:
+            top_left_x = 0
+            top_right_x = 0
+            bottom_left_x = 0
+            bottom_right_x = 0
+        else:
+            top_left_x = int(nonzero_array_top[0])
+            top_right_x = int(nonzero_array_top[-1])
+            bottom_left_x = int(nonzero_array_bottom[0])
+            bottom_right_x = int(nonzero_array_bottom[-1])
     else:
         while index1 < width1:
             if color_array2[:, index1].sum() >= threshold1:
@@ -332,12 +373,18 @@ def line_bisection_coords1(color_array2, pixel_var, orientation_var, height1, wi
         top_right_x = col_right
         bottom_left_x = col_left
         bottom_right_x = col_right
-        nonzero_indices_left = np.nonzero(color_array2[:, col_left])[0]
-        nonzero_indices_right = np.nonzero(color_array2[:, col_right])[0]
-        top_left_y = int(nonzero_indices_left[0])
-        top_right_y = int(nonzero_indices_right[0])
-        bottom_left_y = int(nonzero_indices_left[-1])
-        bottom_right_y = int(nonzero_indices_right[-1])
+        nonzero_array_left = np.nonzero(color_array2[:, col_left])[0]
+        nonzero_array_right = np.nonzero(color_array2[:, col_right])[0]
+        if len(nonzero_array_left) <= 1 or len(nonzero_array_right) <= 1:
+            top_left_y = 0
+            top_right_y = 0
+            bottom_left_y = 0
+            bottom_right_y = 0
+        else:
+            top_left_y = int(nonzero_array_left[0])
+            top_right_y = int(nonzero_array_right[0])
+            bottom_left_y = int(nonzero_array_left[-1])
+            bottom_right_y = int(nonzero_array_right[-1])
 
     array1 = [top_left_x, top_left_y, top_right_x, top_right_y, bottom_right_x, bottom_right_y, bottom_left_x,
               bottom_left_y]
@@ -401,10 +448,19 @@ def band_integer_determ(long_array, short_array):
             array1 = long_array[index1 + 1:index1 + 5]  #Creates an array of the next 4 color values in the array
             median1 = statistics.median(array1)  #Determines the median of the new array
             sum1 = array1.count(median1)  #Determines the number of times the median value of the new array occurs
+            count2s = array1.count(2)
             count1s = array1.count(1)
             count0s = array1.count(0)
+            count8s = array1.count(8)
             sum1and0 = count0s + count1s
-            if sum1 >= 3 and median1 == long_array[index1 + 1]:  #Determines if the change in pixels should result in
+            sum1and2 = count1s + count2s
+            if count8s == 4:
+                short_array.append(long_array[index1 + 1])
+                if index1 <= (length1 - 11) and not (long_array[index1 + 1] == 12 or
+                                                     long_array[index1 + 1] == 11 or long_array[index1 + 1] == 13):
+                    # Checks if index value is less than 10 from max value
+                    index1 += 10  #Jumps 10 pixel in order to leap pass pixel band which it is currently in
+            elif sum1 >= 3 and median1 == long_array[index1 + 1] and median1 != 8:  #Determines if the change in pixels should result in
                 # new pixel being added to the short_array
                 short_array.append(long_array[index1 + 1])  #Adds integer to short_array
                 if index1 <= (length1 - 11) and not (long_array[index1 + 1] == 12 or
@@ -418,6 +474,11 @@ def band_integer_determ(long_array, short_array):
                     short_array.append(1)
                 if index1 <= (length1 - 11):
                     index1 += 10  #Jumps 10 pixel in order to leap pass pixel band which it is currently in
+            elif sum1and2 >= 3:
+                short_array.append(2)
+                if index1 <= (length1 - 11):
+                    index1 += 10
+
         index1 += 1  #Increases increment by 1
 
     # print(long_array)
@@ -522,10 +583,13 @@ def color_array_filler(dimensions1, image_array1, color_array1, x_array1, y_arra
     return None
 
 def critical_points_determ(orientation1, coords, final_slope):
-    if abs(final_slope) < 0.1:
+    if abs(final_slope) < 0.07:
         var1 = 1
         var2 = 1
-    elif final_slope >= 0.1 and final_slope < 0.25:
+    elif final_slope >= 0.07 and final_slope < 0.15:
+        var1 = 1.5
+        var2 = 1
+    elif final_slope >= 0.15 and final_slope < 0.25:
         var1 = 2
         var2 = 1
     elif final_slope >= 0.25 and final_slope < 0.4:
@@ -534,7 +598,10 @@ def critical_points_determ(orientation1, coords, final_slope):
     elif final_slope >= 0.4:
         var1 = 5
         var2 = 1
-    elif final_slope <= -0.1 and final_slope > -0.25:
+    elif final_slope <= -0.07 and final_slope > -0.15:
+        var1 = 1
+        var2 = 1.5
+    elif final_slope <= -0.15 and final_slope > -0.25:
         var1 = 1
         var2 = 2
     elif final_slope <= -0.25 and final_slope > -0.4:
@@ -556,3 +623,21 @@ def critical_points_determ(orientation1, coords, final_slope):
         y2 = int((var2*coords[3] + var1*coords[5]) / sum1)  # Calculates right y value that will be used to determine bisecting line
 
     return x1, y1, x2, y2
+
+def median_blue_determ(image_array, x_values, y_values):
+    length1 = len(x_values)
+    index1 = 0
+    blue_values = []
+    green_values = []
+    while index1 < length1:
+        blue_value_temp = image_array[y_values[index1]][x_values[index1]][2]
+        green_value_temp = image_array[y_values[index1]][x_values[index1]][1]
+        blue_values.append(blue_value_temp)
+        green_values.append(green_value_temp)
+        index1 += 1
+
+    median_blue = np.median(blue_values)
+    median_green = np.median(green_values)
+
+
+    return median_blue, median_green
